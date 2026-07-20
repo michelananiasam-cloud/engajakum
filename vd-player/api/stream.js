611cd3ad54f4a901782486947bb8b24b9badb0f2
michelananiasam-cloud <michel.ananias.am@gmail.com>
2026-07-20 11:39:42 -0300

    Enhance stream.js to check project existence

--- vd-player/api/stream.js
+++ vd-player/api/stream.js
@@ -1,10 +1,21 @@
 const fs = require("fs");
+const path = require("path");
 
 module.exports = (req, res) => {
 
+    const projeto =
+        path.join(
+            process.cwd(),
+            "vd-player"
+        );
+
     res.json({
-        ok: true,
-        arquivos: fs.readdirSync(process.cwd())
+        cwd: process.cwd(),
+        existeProjeto: fs.existsSync(projeto),
+        arquivosProjeto:
+            fs.existsSync(projeto)
+                ? fs.readdirSync(projeto)
+                : []
     });
 
 };
